52cc96ea9be423cc100102af8036ed97fcf8f3f5
Matheus Gois <mattheusgoiis@gmail.com>
2025-07-07 20:40:47 -0300

    feat: event in pull request opened

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,8 @@
 export default (app: Probot) => {
   app.on("pull_request.opened", async (context) => {
     const issueComment = context.issue({
-      body: "Thanks for opening this pull request!",
+      body: "para de encher meu saco",
     });
     await context.octokit.issues.createComment(issueComment);
   });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
